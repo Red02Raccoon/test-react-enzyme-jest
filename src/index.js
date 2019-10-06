0ca86d1ea2eac919883ee06f1e2c50da503b00a9
Nastya Chervona <nastya02chervona@gmail.com>
2019-10-06 15:52:05 +0300

    feat: add prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import { store } from './store'
-import App from './components/App'
-import './index.css'
-
+import { store } from "./store";
+import App from "./components/App";
+import "./index.css";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,4 @@
 export default {
-    addUser : (name, email, number) => fetch(`www.someurl.com`).then(res => res.json().catch((err) => console.log('error', err)))
-}
+	addUser: (name, email, number) =>
+		fetch(`www.someurl.com`).then(res => res.json().catch(err => console.log("error", err))),
+};
